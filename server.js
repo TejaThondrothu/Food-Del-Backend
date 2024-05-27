27eb6c027848f839bbf23ae6e46acc715ed0988a
TejaThondrothu <thondrothuteja1997@gmail.com>
2024-05-27 18:38:51 +0530

    Implemented the functionality for cart API's with middleware with JWT token system

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import foodRouter from "./routes/foodRoute.js"
 import userRouter from "./routes/userRoute.js"
 
 import 'dotenv/config'
+import cartRouter from "./routes/cartRoute.js"
 
 
 
@@ -25,6 +26,8 @@ app.use("/api/food",foodRouter)
 app.use("/images",express.static('uploads'))  //folder name :uploads
 
 app.use("/api/user",userRouter)
+
+app.use("/api/cart",cartRouter)
 
 // app.get("/",(req,res)=>{
 //     res.send("API is working")
